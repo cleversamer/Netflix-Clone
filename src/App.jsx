84afa9f5389aee69a0ca7fa9f82ac3d0096e06eb
Samer A <thedev.samer@gmail.com>
2022-04-05 13:22:59 +0300

    finished building the row component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
+import requests from "./requests";
+import Row from "./components/Row";
 import "./css/app.css";
 
 const App = () => {
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        large
+      />
+
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
---
+++ src/components/Row.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "../axios";
+import "../css/row.css";
+
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+
+const Row = ({ title, fetchUrl, large }) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    // axios
+    //   .get(fetchUrl)
+    //   .then((res) => setMovies(res.data.results))
+    //   .catch((err) => {});
+  }, []);
+
+  const getPosterClasses = () => {
+    return `row__poster ${large ? "row__poster--large" : ""}`;
+  };
+
+  return (
+    <section className="row">
+      <h2 className="row__heading">{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={getPosterClasses()}
+            src={baseImgUrl + movie.poster_path}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Row;
